ac9ed52be6543d09f18c831b6b0fa2e97fd8c25c
yuvalt-p <yuvaltomer2000@gmail.com>
2026-02-07 13:53:33 +0200

    first commit

---
+++ Parser.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+
+export default class Parser{
+    constructor (inputFile) {
+        this.rawFile = inputFile;
+        this.fileAsStringArray = this.convertInputFileToStringArray();
+    
+    }
+    convertInputFileToStringArray() {
+        const fileAsStringArray = fs.readFileSync(this.rawFile, 'utf-8').split('\n');
+        return fileAsStringArray;
+    }
+
+
+
+
+
+}
