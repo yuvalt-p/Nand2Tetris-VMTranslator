5f058149efa078b077c10d10bf1bcc04393bd91a
yuvalt-p <yuvaltomer2000@gmail.com>
2026-02-13 15:08:36 +0200

    created main vm translator class + added a package.json inorder to run as a script

---
+++ Main.js
@@ -0,0 +1,41 @@
+import Parser from "./Parser.js";
+import CodeWriter from "./CodeWriter.js";
+
+export default class VMTranslator {
+  constructor(inputPath, outputPath) {
+    this.parsedFileObject = new Parser(inputPath);
+    this.codeWriter = new CodeWriter(outputPath);
+  }
+
+  parseEachCommandIntoAssembly() {
+    for (let commandLine of this.parsedFileObject.fileAsStringArray) {
+      if (this.parsedFileObject.hasMoreCommands()) {
+        this.parsedFileObject.advance();
+        let commandType = this.parsedFileObject.commandType();
+        let commandOrSegment = this.parsedFileObject.arg1();
+        let memoryIndex = this.parsedFileObject.arg2();
+        if (commandType === "C_ARITHMETIC") {
+          this.codeWriter.writeArithmetic(commandOrSegment);
+        } else if (commandType === "C_POP" || commandType === "C_PUSH") {
+          this.codeWriter.writePushPop(
+            commandType,
+            commandOrSegment,
+            memoryIndex,
+          );
+        }
+      } else {
+        this.codeWriter.close();
+        console.log(
+          `no more commands. file has been created at ${this.codeWriter.outputFile.path}`,
+        );
+        return;
+      }
+    }
+  }
+}
+
+const inputPath = process.argv[2];
+const outputPath = process.argv[3];
+
+const translator = new VMTranslator(inputPath, outputPath);
+translator.parseEachCommandIntoAssembly();
